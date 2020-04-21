e1b70fa2046fbeb9217b46f551492297d4e27096
AndrLimarenko <62445399+AndrLimarenko@users.noreply.github.com>
2020-04-22 00:26:07 +0300

    Add files via upload

---
+++ Task_4_1/main/java/Man.java
@@ -0,0 +1,24 @@
+public class Man {
+    double weight;
+    double height;
+
+    public Man(double weight, double height) {
+        this.height =height;
+        this.weight=weight;
+    }
+    public void imtManCalcuation () {
+        double imt;
+        double ideal = Math.abs(height - 100);
+        imt = weight / ((height / 100) * (height / 100));
+        System.out.println("IMT коэфициент:"+imt);
+        if ((imt >= 20) & (imt <= 25))
+            System.out.println("нормально все с весом");
+        if (imt > 25)
+            System.out.println("лучше похудеть на " + (weight - ideal) + " килограмм");
+        if (imt < 20)
+            System.out.println("лучше набрать вес на " + (ideal - weight) + " килограмм");
+        System.out.println();
+    }
+}
+
+
